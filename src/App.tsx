b9bf8a044e89611db9277870a22bf85ccb63e2c4
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-08 22:19:12 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <div>
+      <p>Hello, & Dom!</p>
+      <footer>
+        <img src="icons/footer/favicon.png" alt="Monk" />
+      </footer>
+    </div>
+  )
+}
